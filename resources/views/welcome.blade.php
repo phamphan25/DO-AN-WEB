56f7a13f76bf0b15439a12be8e087a8b281cb824
nhat tan <nhattanidilian@gmail.com>
2024-11-21 18:46:13 +0700

    trang thông tin website và đơn hàng

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.info')
+@section('title', 'Trang chu - Online Store')
+@section('content')
+<div class="text-center">
+Chao mung den voi ung dung Online Store!
+</div>
+@endsection
